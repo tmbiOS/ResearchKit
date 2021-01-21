51374f8c8fcc643d4450733a3edb0c883f0bfafc
Igor <tamboff69@gmail.com>
2021-01-21 16:17:23 +0300

    bug: fixed the survey bg color

--- ResearchKit/Common/ORKFormStepViewController.m
+++ ResearchKit/Common/ORKFormStepViewController.m
@@ -580,7 +580,7 @@ static const CGFloat DelayBeforeAutoScroll = 0.25;
             }
             else {
                 if (@available(iOS 13.0, *)) {
-                    UIColor* color = [UIColor colorNamed:@"bg-color"];
+                    UIColor* color = [UIColor colorNamed:@"mainBackgroundColor"];
                     [_tableView setBackgroundColor:color];
                 } else {
                     [_tableView setBackgroundColor:ORKColor(ORKBackgroundColorKey)];
